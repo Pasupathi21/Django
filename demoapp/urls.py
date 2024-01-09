cfc14b5c73c76003571f188c87f48342c5ee2d49
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2024-01-09 23:30:23 +0530

    new app created in django

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Just include the sub app route to main app
+    path('/challenges', include('challenges.urls'))
 ]
